031670d189526c354bd90dfa935095dc439356f7
a-pedro <116759801+a-pedro@users.noreply.github.com>
2023-06-01 17:19:28 +0200

    Add files via upload

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,22 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }, {
+    "artist": "Some Band", 
+    "title": "New Album",
+    "release_year": 2013,
+    "formats": [ 
+      "CD", 
+      "Cassette", 
+      "LP"
+    ],
+  }
+];
